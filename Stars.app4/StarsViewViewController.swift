ac939b422d82e4a4dc3cd395ddf48953caca79e6
Osha Washington <washingtonosha209@yahoo.com>
2019-12-05 19:09:01 -0800

    Views

---
+++ Stars.app4/StarsViewViewController.swift
@@ -0,0 +1,33 @@
+//
+//  StarsViewViewController.swift
+//  Stars.app4
+//
+//  Created by Osha Washington on 12/5/19.
+//  Copyright © 2019 Osha Washington. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewViewController: UIViewController {
+    
+    // Mark: IBOutlets
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var distanceTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+// Mark IBActions
+    @IBAction func printStars(_ sender: UIButton) {
+    }
+    @IBAction func createStar(_ sender: UIButton) {
+    }
+    
+
+}
+
